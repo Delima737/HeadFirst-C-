5c2cb57a2b3dad5532aa2dbfe8acbfd240ac09c8
Lucas Lima <72763196+Delima737@users.noreply.github.com>
2024-02-07 11:30:06 -0300

    .

--- Contact/Contact/Program.cs
+++ Contact/Contact/Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/// <summary>
+/// These using lines are at the top of every code file.They tell
+/// C# to use all of those .NET Framework classes. Each one tells
+/// your program that the classes in this particular.cs file will use all
+/// of the classes in one specific .NET Framework namespace.
+    /// </summary>
 namespace Contact
 {
     static class Program
