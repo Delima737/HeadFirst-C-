2d9bf670e80d321ad3d5562978fc30ae026e7481
Lucas Lima <72763196+Delima737@users.noreply.github.com>
2024-02-19 18:09:58 -0300

    update

---
+++ POO/POO exp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO_exp
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+		{
+		class Clown
+		{
+			public string Name;
+			public int Height;
+			public void TalkAboutYourself()
+			{
+				MessageBox.Show("My name is "
+	+ Name + " and I’m " + Height + " inches tall.");
+				{
+					Clown oneClown = new Clown();
+					oneClown.Name = "Boffo";
+					oneClown.Height = 14;
+					oneClown.TalkAboutYourself();
+					Clown anotherClown = new Clown();
+					anotherClown.Name = "Biff";
+					anotherClown.Height = 16;
+					anotherClown.TalkAboutYourself();
+					Clown clown3 = new Clown();
+					clown3.Name = anotherClown.Name;
+					clown3.Height = oneClown.Height - 3;
+					clown3.TalkAboutYourself();
+					anotherClown.Height *= 2;
+					anotherClown.TalkAboutYourself();
+				}
+			}
+		}
